05d2d64c4214f7224c005f01d5838f0da0ba63be
Xuan4781 <123594280+Xuan4781@users.noreply.github.com>
2026-02-15 14:23:21 -0500

    add playermovement script and change new input to old input

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace SugarRun
+{
+	public class PlayerMovement : MonoBehaviour
+	{
+		public float speed = 4f;
+		// Start is called once before the first execution of Update after the MonoBehaviour is created
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			float x = Input.GetAxis("Horizontal");
+			float z = Input.GetAxis("Vertical");
+
+			Vector3 move = new Vector3(x, 0, z);
+			transform.Translate(move * speed * Time.deltaTime);
+		}
+	}
+
+}

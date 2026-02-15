4c1d9fde3a4e06b07193e5ddf7363f9ff8b04555
Xuan4781 <123594280+Xuan4781@users.noreply.github.com>
2026-02-15 14:09:00 -0500

    script for guard initial point

---
+++ Assets/Scripts/GuardAI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace SugarRun
+{
+	public class GuardUI : MonoBehaviour
+	{
+
+		public Transform pointA;
+		public Transform pointB;
+
+		private NavMeshAgent agent;
+		private bool goingToB = true;
+		// Start is called once before the first execution of Update after the MonoBehaviour is created
+		void Start()
+		{
+			agent = GetComponent<NavMeshAgent>();
+			agent.SetDestination(pointA.position);
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
+
